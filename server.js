0a19819689d7e64fc3d57f8deac8c74fb1f7ef39
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2020-09-18 17:49:38 +0200

    updates to deploy to huroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/galerie96'));
+app.use(express.static(__dirname + '/bookshelves'));
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/galerie96/index.html'));
+    res.sendFile(path.join(__dirname+'/bookshelves/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
